b58e600f7e3d5ba076f4703ca925aea18a4fab4c
jack_titzman <jack_titzman@baylor.edu>
2018-04-26 23:45:36 -0500

    javadocs for assignmentserviceinterface

--- src/service/AssignmentServiceInterface.java
+++ src/service/AssignmentServiceInterface.java
@@ -6,7 +6,27 @@ import data.Assignment;
 import data.Class;
 
 public interface AssignmentServiceInterface {
+	/**
+	 * gets all the assignments from a class
+	 * @author titzman
+	 * @param c the class to get assignments from
+	 * @return list of assignments from class
+	 */
 	public List<Assignment> getAssignments(Class c);
+	/**
+	 * saves the assignment to the given class
+	 * @author titzman
+	 * @param assignment the assignment to save
+	 * @param c the class to save the assignment to
+	 * @return a serviceresponse for saving assignments
+	 */
 	public ServiceResponse saveAssignment(Assignment assignment, Class c);
+	/**
+	 * delete the assignment from the given class
+	 * @author titzman
+	 * @param assignment the assignment to delete
+	 * @param c the class to delete the assignment from
+	 * @return a serviceresponse for deleting assignments
+	 */
 	public ServiceResponse deleteAssignment(Assignment assignment, Class c);
 }
